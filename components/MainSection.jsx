e2a091c44267f93caae3f631e9be578d84c56263
cyhammad <hammad727272@gmail.com>
2025-08-08 22:47:41 +0500

    image changed

--- components/MainSection.jsx
+++ components/MainSection.jsx
@@ -3,7 +3,7 @@ import CallButtons from "./CallButtons";
 
 const MainSection = () => {
   return (
-    <div className="w-full flex flex-col relative items-center justify-center bg-[url(/kitchen.jpg)] bg-cover bg-fixed min-h-[90vh] md:min-h-[80vh] h-full bg-center">
+    <div className="w-full flex flex-col relative items-center justify-center bg-[url(/appliances.png)] bg-cover bg-fixed min-h-[90vh] md:min-h-[80vh] h-full bg-center">
       <div className="flex flex-col items-center justify-center w-full gap-5 h-full min-h-[90vh] md:min-h-[80vh] px-5 py-10 bg-black/70">
         <div className="flex flex-col gap-5 max-w-7xl w-full">
           <h1 className="text-white text-4xl leading-tight md:text-5xl font-bold">
@@ -16,7 +16,7 @@ const MainSection = () => {
             We are a team of experienced technicians who are dedicated to
             providing the best possible service to our customers. We offer
             quick, reliable, and affordable repairs for all appliances. We offer
-            a wide range of services for all appliances of all brands.
+            a wide range of services for home appliances.
           </p>
           <CallButtons />
         </div>

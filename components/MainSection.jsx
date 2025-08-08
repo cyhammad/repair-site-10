817fa544f9c33a21abaa90944809575a163c0757
cyhammad <hammad727272@gmail.com>
2025-08-09 02:03:19 +0500

    tw

--- components/MainSection.jsx
+++ components/MainSection.jsx
@@ -3,7 +3,11 @@ import CallButtons from "./CallButtons";
 
 const MainSection = () => {
   return (
-    <div className="w-full flex flex-col relative items-center justify-center bg-[url(/appliances.png)] bg-cover bg-fixed min-h-[90vh] md:min-h-[80vh] h-full bg-center">
+    <div className="w-full flex flex-col relative items-center justify-center bg-cover bg-center min-h-[90vh] md:min-h-[80vh] h-full"
+         style={{
+           backgroundImage: "url(/appliances.png)",
+           backgroundAttachment: "scroll"
+         }}>
       <div className="flex flex-col items-center justify-center w-full gap-5 h-full min-h-[90vh] md:min-h-[80vh] px-5 py-10 bg-black/70">
         <div className="flex flex-col gap-5 max-w-7xl w-full">
           <h1 className="text-white text-4xl leading-tight md:text-5xl font-bold">
